3287a62a0eea1874c0c8c77d4731bdf6730b5470
vrmthdrth <vitaliyalexandrov1997@gmail.com>
2020-03-05 14:02:36 +0400

    deletion from master

--- homework-01/src/part1/lesson01/task02/Main.java
+++
@@ -1,7 +0,0 @@
-package part1.lesson01.task02;
-
-public class Main {
-    public static void main(String[] args) {
-        Methods.mainCommand();
-    }
-}
--- homework-01/src/part1/lesson01/task03/Main.java
+++
@@ -1,7 +0,0 @@
-package part1.lesson01.task03;
-
-public class Main {
-    public static void main(String[] args) {
-        SortHelper.mainCommand();
-    }
-}
