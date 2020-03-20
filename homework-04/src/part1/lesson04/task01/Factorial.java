f18bc314004b0432b85ad25e4b3c5e4c36a9bc29
vrmthdrth <vitaliyalexandrov1997@gmail.com>
2020-03-20 18:23:22 +0400

    javadoc added

--- homework-04/src/part1/lesson04/task01/Factorial.java
+++ homework-04/src/part1/lesson04/task01/Factorial.java
@@ -5,7 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 
+/**
+ * Contains static methods which are necessary to compute factorials
+ */
 public class Factorial{
+    /**
+     * Computes a factorial of integer number
+     * @param number Number
+     * @return Result as a BigInteger instance
+     */
     public static BigInteger compute(int number){
         BigInteger result = BigInteger.valueOf(1);
         for(int i = 1; i <= number; i++){
@@ -14,17 +22,30 @@ public class Factorial{
         return result;
     }
 
+    /**
+     * Computes factorials of every number from input integer array
+     * @param array Array
+     * @return Result as a List of BigInteger
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     public static List<BigInteger> computeFactorialsForIntegerArray(int[] array) throws ExecutionException, InterruptedException {
         List<Future<BigInteger>> futureList = new ArrayList<>();
         ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() + 1);
-        for(int i = 0; i < array.length; i++){
-            final int value = array[i];
+        for(int value: array){
             futureList.add(CompletableFuture.supplyAsync(() -> Factorial.compute(value), executorService));
         }
         executorService.shutdown();
         return getFutureValues(futureList);
     }
 
+    /**
+     * Gets values from list of futures. Writes them into List of BigInteger
+     * @param futureList Future list
+     * @return List of BigInteger
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     public static List<BigInteger> getFutureValues(List<Future<BigInteger>> futureList) throws ExecutionException, InterruptedException {
         List<BigInteger> values = new ArrayList<>();
         for(Future<BigInteger> future: futureList){
--- homework-04/src/part1/lesson04/task01/Helper.java
+++ homework-04/src/part1/lesson04/task01/Helper.java
@@ -5,7 +5,15 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+/**
+ * Class containing necessary static methods.
+ */
 public class Helper {
+    /**
+     * Generates random integer array.
+     * @param arraySize Size of array
+     * @return New random array
+     */
     public static int[] generateRandomIntegerArray(int arraySize){
         int[] randomArray = new int[arraySize];
         for(int i = 0; i < arraySize; i++){
@@ -14,6 +22,11 @@ public class Helper {
         return randomArray;
     }
 
+    /**
+     * Displays result of calculation.
+     * @param originalArray Original array
+     * @param bigIntegerList Result of calculation
+     */
     public static void displayResult(int[] originalArray, List<BigInteger> bigIntegerList){
         Iterator iterator = bigIntegerList.iterator();
         for(int originalValue: originalArray){
@@ -21,6 +34,11 @@ public class Helper {
         }
     }
 
+    /**
+     * A procedure to execute in main().
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     public static void mainProcedure() throws ExecutionException, InterruptedException {
         int[] randomArray = Helper.generateRandomIntegerArray(1000);
         List<BigInteger> resultList = Factorial.computeFactorialsForIntegerArray(randomArray);
